18c8b599055e8a3178043232bc85361de1db60e6
EstebanRios99 <esteban.rios@epn.edu.ec>
2020-09-05 20:20:23 -0500

    #4 Inicio de sesión y registro

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import Login from './pages/Login';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,12 +23,13 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+
+const App = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/login" component={Login} exact={true} />
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
---
+++ src/constants/routes.tsx
@@ -0,0 +1,24 @@
+const publicRoutes = {
+    LOGIN: '/ingreso',
+    //REGISTER: '/registro',
+    //ARTICLES: '/articulos',
+    //USERS: '/usuarios',
+    //USERS_ID: `/usuario/:id`,
+    //HOME: '/',
+    //ABOUT: '/acerca-de',
+    //ANTD: '/antd',
+    //PROFILE: '/perfil'
+  
+  };
+  
+  const privateRoutes = {
+    LOGOUT: '/logout',
+    PRIVATE: '/privada',
+    //ARTICLE_ID: '/articulo/:id'
+  };
+  
+  const Routes = {
+    ...publicRoutes,
+    ...privateRoutes
+  };
+  export default Routes;
