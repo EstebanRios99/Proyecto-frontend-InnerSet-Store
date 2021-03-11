dfdd69e3f30de44207c7a033f487240e3332b4e4
EstebanRios99 <esteban.rios@epn.edu.ec>
2021-03-11 00:16:20 -0500

    Cambios en la presentación de la página de reportes

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import {
-    IonButton, IonCol,
+    IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonDatetime,
     IonGrid,
-    IonHeader, IonItem,
+    IonHeader, IonImg, IonItem, IonLabel,
     IonPage, IonRow,
     IonTitle,
     IonToolbar,
@@ -10,9 +10,15 @@ import {
 import Layouts from '../components/Layout';
 import '../styles/app.css';
 import "../theme/toolbar.css";
+import moment from 'moment';
+import sale from '../images/ventas.png';
+import product from '../images/productos.png';
+import stock from '../images/stock.png';
 
 const Reports = () => {
 
+    const [startDate, setStartDate]=useState('');
+    const [endDate, setEndDate]=useState('');
 
     return    (
         <>
@@ -25,24 +31,57 @@ const Reports = () => {
                         <IonTitle id={"letter"}>Reportes</IonTitle>
                     </IonToolbar>
                 </IonHeader>
+
+                <IonItem>
+                    <IonLabel><strong>Fecha de Inicio</strong></IonLabel>
+                    <IonDatetime displayFormat="DD MMM YYYY" placeholder="Seleccione Fecha" value={startDate} onIonChange={e => setStartDate(e.detail.value)}/>
+                </IonItem>
+                <IonItem>
+                    <IonLabel><strong>Fecha de Fin</strong></IonLabel>
+                    <IonDatetime displayFormat="DD MMM YYYY" placeholder="Seleccione Fecha" value={endDate} onIonChange={e => setEndDate(e.detail.value)}/>
+                </IonItem>
                 <IonGrid>
                     <IonRow>
-                        <IonCol/>
                         <IonCol>
-                            <a href="http://localhost:8000/api/pdf/requests" id={"text"}>
-                                <IonButton id={"btn"}>Reporte de Pedidos</IonButton>
+                            <a href={`http://localhost:8000/api/pdf/requests/${moment(startDate).format('YYYYMMDD')}/${moment(endDate).format('YYYYMMDD')}`} id={"text"}>
+                                <IonCard>
+                                    <IonImg src={sale}
+                                            style={{height: "100px"}}/>
+                                    <IonCardHeader>
+                                        <IonCardSubtitle>Documento</IonCardSubtitle>
+                                        <IonCardTitle>Reporte de Pedidos</IonCardTitle>
+                                    </IonCardHeader>
+                                </IonCard>
                             </a>
                         </IonCol>
-                        <IonCol/>
+                        <IonCol>
+                            <a href={`http://localhost:8000/api/pdf/products/${moment(startDate).format('YYYYMMDD')}/${moment(endDate).format('YYYYMMDD')}`} id={"text"}>
+                                <IonCard>
+                                    <IonImg src={product}
+                                            style={{height: "100px"}}/>
+                                    <IonCardHeader>
+                                        <IonCardSubtitle>Documento</IonCardSubtitle>
+                                        <IonCardTitle>Reporte de Productos</IonCardTitle>
+                                    </IonCardHeader>
+                                </IonCard>
+                            </a>
+                        </IonCol>
                     </IonRow>
                     <IonRow>
-                        <IonCol/>
+                        <IonCol size={3}/>
                         <IonCol>
-                            <a href="http://localhost:8000/api/pdf/products" id={"text"}>
-                            <IonButton id={"btn"}>Reporte de Productos</IonButton>
+                            <a href="http://localhost:8000/api/pdf/stock" id={"text"}>
+                                <IonCard>
+                                    <IonImg src={stock}
+                                            style={{height: "100px"}}/>
+                                    <IonCardHeader>
+                                        <IonCardSubtitle>Documento</IonCardSubtitle>
+                                        <IonCardTitle>Stock</IonCardTitle>
+                                    </IonCardHeader>
+                                </IonCard>
                             </a>
                         </IonCol>
-                        <IonCol/>
+                        <IonCol size={3}/>
                     </IonRow>
                 </IonGrid>
             </IonPage>
